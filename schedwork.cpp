99c45872f380e83841407f4c004b6ae39e7b450b
Omar Abd-Elhamid <oabdelha@usc.edu>
2023-04-12 17:03:31 -0700

    partially working

--- schedwork.cpp
+++ schedwork.cpp
@@ -22,6 +22,7 @@ static const Worker_T INVALID_ID = (unsigned int)-1;
 
 // Add prototypes for any helper functions here
 
+bool scheduleinator (int r, int c, const AvailabilityMatrix& avail, const size_t maxShifts, DailySchedule& sched, vector<int> tracker);
 
 // Add your implementation of schedule() and other helper functions here
 
@@ -36,10 +37,53 @@ bool schedule(
         return false;
     }
     sched.clear();
-    // Add your code below
 
+    //Shift tracker
+    vector<int> tracker (avail.size(), 0);
 
+    //Create blank schedule in sched
+    vector<Worker_T> row (dailyNeed, INVALID_ID);
+    vector<vector<Worker_T>> newsched (avail[0].size(), row);
+    sched = newsched;
 
+    return scheduleinator (0, 0, avail, maxShifts, sched, tracker);
+}
 
+bool scheduleinator(int r, int c, const AvailabilityMatrix& avail, const size_t maxShifts, DailySchedule& sched, vector<int> tracker){
+    if (r == sched.size()) return true;
+    if (c == sched[0].size()){
+        return scheduleinator(r+1, 0, avail, maxShifts, sched, tracker);
+    }
+
+    if (sched[r][c] != INVALID_ID){
+        scheduleinator(r, c+1, avail, maxShifts, sched, tracker);
+    }
+
+    for (size_t i = 0; i < avail.size(); i++){
+        //Feeling good
+        sched[r][c] = i;
+        if (tracker[i] >= maxShifts){
+            sched[r][c] = INVALID_ID;
+            continue;
+        }
+        if (!avail[i][r]){
+            sched[r][c] = INVALID_ID;
+            continue;
+        }
+
+        tracker[i]++;
+
+        //???
+        if (!scheduleinator(r, c+1, avail, maxShifts, sched, tracker)){
+            sched[r][c] = INVALID_ID;
+            tracker[i]--;
+            continue;
+        }
+        else{
+            return true;
+        }
+    }
+
+    return false;
 }
 
--- wordle.cpp
+++ wordle.cpp
@@ -5,6 +5,7 @@
 #include <algorithm> 
 #include <map>
 #include <set>
+#include <vector>
 #endif
 
 #include "wordle.h"
@@ -13,7 +14,8 @@ using namespace std;
 
 
 // Add prototypes of helper functions here
-
+void wordinator(const string& in, const string& floating, const set<std::string>& dict, int len, set<string>& finaldict, string word, vector<char> floatingChars);
+size_t calcbars(const std::string& in, int len);
 
 // Definition of primary wordle function
 std::set<std::string> wordle(
@@ -22,7 +24,87 @@ std::set<std::string> wordle(
     const std::set<std::string>& dict)
 {
     // Add your code here
+    std::set<std::string> finaldict;
 
+    //Setup by length
+    /*
+    for(set<string>::iterator it = dict.begin(); it != dict.end(); it++){
+        if (it->length() == in.length()){
+            finaldict.insert(*it);
+        }
+    }
+     */
+
+    vector<char> floatingChars;
+
+    for(size_t i = 0; i < floating.size(); i++) {
+        floatingChars.push_back(floating[i]);
+    }
+    cout << "test " << endl;
+    wordinator(in, floating, dict, 0, finaldict, "", floatingChars);
+    return finaldict;
 }
 
 // Define any helper functions here
+
+void wordinator(const std::string& in, const std::string& floating, const std::set<std::string>& dict, int len, set<string>& finaldict, string word, vector<char>floatingChars){
+
+    //Base Case
+    cout << "Hi" << endl;
+    if (len == in.length()){
+        if (dict.find(word)!=dict.end()) {
+            finaldict.insert(word);
+        }
+        return;
+    }
+
+    //If current index has letter, add letter
+    if (in[len] != '-'){
+        word+= in[len];
+        wordinator(in, floating, dict, len+1, finaldict, word, floatingChars);
+    }
+    else {
+        if(calcbars(in, len) == floatingChars.size()) {
+            vector<char> temp = floatingChars;
+            for (size_t i = 0; i < floatingChars.size(); i ++){
+                word += floatingChars[i];
+                temp.erase(temp.begin()+i);
+                wordinator(in, floating, dict, len+1, finaldict, word, floatingChars);
+                floatingChars = temp;
+            }
+        }
+        else {
+            vector<char>temp = floatingChars;
+            for (int i = 97; i < 123; i++) {
+                vector<char>::iterator charloc = find(floatingChars.begin(), floatingChars.end(), char(i));
+                if (charloc != floatingChars.end()) {
+                    cout <<"seg fault b" << endl;
+                    temp.erase(charloc);
+                    word += char(i);
+                    wordinator(in, floating, dict, len + 1, finaldict, word, floatingChars);
+                    floatingChars = temp;
+                }
+                else {
+                    cout << "seg fault a" << endl;
+                    word += char(i);
+                    wordinator(in, floating, dict, len+1, finaldict, word, floatingChars);
+                }
+            }
+        }
+    }
+
+
+
+}
+
+
+size_t calcbars(const std::string& in, int len){
+    int dashes = 0;
+    for (size_t i = len; i < in.length(); i++){
+        if (in[i] == '-'){
+            dashes++;
+        }
+    }
+
+    return dashes;
+}
